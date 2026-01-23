8243899b70d4d34238f88cb93cb663ef4852e1a2
Jamshed Khan <90842589+jamedkh@users.noreply.github.com>
2026-01-23 12:50:24 +0500

    tailwind added

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './globals.css'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  title: 'Payload Blog Template',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 export default async function HomePage() {
   return (
     <div>
-      <h1>Welcome to Payload</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   )
 }
